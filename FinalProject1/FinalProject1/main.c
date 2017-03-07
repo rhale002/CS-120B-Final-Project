2c57ac7302cffb8769b971d266814931a614496f
Robyn Haley <rhale002@student.ucr.edu>
2017-03-07 11:37:59 -0800

    First Commit

---
+++ FinalProject1/FinalProject1/main.c
@@ -0,0 +1,19 @@
+/*
+ * FinalProject1.c
+ *
+ * Created: 2/28/2017 12:45:49 PM
+ * Author : robyn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
