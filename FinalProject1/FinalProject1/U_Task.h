78a3d715e194b79406bdf9a0f8f7413f15562f34
Robyn Haley <rhale002@student.ucr.edu>
2017-03-09 13:26:13 -0800

    Started working on USART Code

--- FinalProject1/FinalProject1/U_Task.h
+++ FinalProject1/FinalProject1/U_Task.h
@@ -6,14 +6,15 @@ enum U_States {U_Init, U_Start, U_Base} U_State;
 unsigned long U_Period = 1;
 
 //Create extern variables for all bytes to be sent
-/* 
-extern unsigned char test;
-*/
+extern Player player;
+Obstacle obstacles[8];
 
 //Initialize U_Task Tick Function
 int U_Tick(int currentState)
 {
-	static int USART_Index;
+	static unsigned char USART_Index;
+	static unsigned char USART_Check;
+	static unsigned char obstacleIndex;
 	
 	switch(currentState)
 	{
@@ -44,23 +45,128 @@ int U_Tick(int currentState)
 		case U_Init:
 		{
 			USART_Index = 0;
+			USART_Check = 0;
+			obstacleIndex = 0;
 		}
 		break;
 		case U_Base:
 		{
-			//Example code of how USART will send bytes
-			/*
-			if(USART_IsSendReady(0) && USART_Index == 0)
+			if(USART_IsSendReady(0) && USART_Index == USART_Check)			//Send byte over USART for player x-position
 			{
-				USART_Send(test, 0);
+				USART_Send(player.xPosition, 0);
 				++USART_Index;
+				++USART_Check;
 			}
-			else if(USART_IsSendReady(0) && USART_Index == 1)
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for player y-position
 			{
-				//Send B1
-				USART_Index = 0;
+				USART_Send(player.yPosition, 0);
+				++USART_Index;
+				++USART_Check;
 			}
-			*/
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 0 x-position
+			{
+				USART_Send(obstacles[obstacleIndex].xPosition, 0);
+				++USART_Index;
+				++USART_Check;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 0 y-position
+			{
+				USART_Send(obstacles[obstacleIndex].yPosition, 0);
+				++USART_Index;
+				++USART_Check;
+				++obstacleIndex;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 1 x-position
+			{
+				USART_Send(obstacles[obstacleIndex].xPosition, 0);
+				++USART_Index;
+				++USART_Check;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 1 y-position
+			{
+				USART_Send(obstacles[obstacleIndex].yPosition, 0);
+				++USART_Index;
+				++USART_Check;
+				++obstacleIndex;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 2 x-position
+			{
+				USART_Send(obstacles[obstacleIndex].xPosition, 0);
+				++USART_Index;
+				++USART_Check;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 2 y-position
+			{
+				USART_Send(obstacles[obstacleIndex].yPosition, 0);
+				++USART_Index;
+				++USART_Check;
+				++obstacleIndex;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 3 x-position
+			{
+				USART_Send(obstacles[obstacleIndex].xPosition, 0);
+				++USART_Index;
+				++USART_Check;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 3 y-position
+			{
+				USART_Send(obstacles[obstacleIndex].yPosition, 0);
+				++USART_Index;
+				++USART_Check;
+				++obstacleIndex;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 4 x-position
+			{
+				USART_Send(obstacles[obstacleIndex].xPosition, 0);
+				++USART_Index;
+				++USART_Check;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 4 y-position
+			{
+				USART_Send(obstacles[obstacleIndex].yPosition, 0);
+				++USART_Index;
+				++USART_Check;
+				++obstacleIndex;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 5 x-position
+			{
+				USART_Send(obstacles[obstacleIndex].xPosition, 0);
+				++USART_Index;
+				++USART_Check;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 5 y-position
+			{
+				USART_Send(obstacles[obstacleIndex].yPosition, 0);
+				++USART_Index;
+				++USART_Check;
+				++obstacleIndex;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 6 x-position
+			{
+				USART_Send(obstacles[obstacleIndex].xPosition, 0);
+				++USART_Index;
+				++USART_Check;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 6 y-position
+			{
+				USART_Send(obstacles[obstacleIndex].yPosition, 0);
+				++USART_Index;
+				++USART_Check;
+				++obstacleIndex;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 7 x-position
+			{
+				USART_Send(obstacles[obstacleIndex].xPosition, 0);
+				++USART_Index;
+				++USART_Check;
+			}
+			else if(USART_IsSendReady(0) && USART_Index == USART_Check)		//Send byte over USART for obstacle 7 y-position
+			{
+				USART_Send(obstacles[obstacleIndex].yPosition, 0);
+				++USART_Index;
+				++USART_Check;
+				++obstacleIndex;
+			}
 		}
 		break;
 		
