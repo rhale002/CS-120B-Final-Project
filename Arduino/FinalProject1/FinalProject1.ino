4af511a5d3f1cbca3a7ab1f1059dea302e05917c
Robyn Haley <rhale002@student.ucr.edu>
2017-03-13 00:36:10 -0700

    Made the maze

--- Arduino/FinalProject1/FinalProject1.ino
+++ Arduino/FinalProject1/FinalProject1.ino
@@ -53,18 +53,84 @@ void drawBoundaries()
    matrix.drawLine(31, 13, 18, 13, matrix.Color333(7, 0, 0));
    matrix.drawLine(18, 13, 18, 1, matrix.Color333(7, 0, 0));
    matrix.drawLine(17, 1, 17, 14, matrix.Color333(7, 0, 0));
-   matrix.drawLine(18, 14, 31, 14, matrix.Color333(7, 0, 0));
    matrix.drawLine(30, 0, 20, 0, matrix.Color333(7, 0, 0));
-   matrix.drawLine(30, 14, 31, 14, matrix.Color333(7, 0, 0));
    matrix.drawLine(30, 1, 30, 11, matrix.Color333(7, 0, 0));
    matrix.drawLine(29, 1, 29, 11, matrix.Color333(7, 0, 0));
    matrix.drawLine(28, 1, 28, 11, matrix.Color333(7, 0, 0));
+   matrix.drawLine(24, 13, 24, 2, matrix.Color333(7, 0, 0));
+   matrix.drawLine(23, 13, 23, 2, matrix.Color333(7, 0, 0));
+   matrix.drawLine(22, 13, 22, 2, matrix.Color333(7, 0, 0));
+   matrix.drawLine(21, 13, 21, 2, matrix.Color333(7, 0, 0));
 
    //Right lower corner maze
    matrix.drawLine(18, 1, 1, 1, matrix.Color333(7, 0, 0));
    matrix.drawLine(0, 3, 15, 3, matrix.Color333(7, 0, 0));
    matrix.drawLine(1, 5, 16, 5, matrix.Color333(7, 0, 0));
    matrix.drawLine(0, 7, 15, 7, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 9, 16, 9, matrix.Color333(7, 0, 0));
+   matrix.drawLine(0, 11, 15, 11, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 13, 16, 13, matrix.Color333(7, 0, 0));
+
+   //Upper Half line
+   matrix.drawLine(31, 14, 1, 14, matrix.Color333(7, 0, 0));
+
+   //Upper right L
+   matrix.drawLine(1, 17, 5, 17, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 18, 5, 18, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 19, 5, 19, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 20, 11, 20, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 21, 11, 21, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 22, 11, 22, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 23, 11, 23, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 24, 11, 24, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 25, 11, 25, matrix.Color333(7, 0, 0));
+
+   //Upper right top bar
+   matrix.drawLine(1, 28, 18, 28, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 29, 18, 29, matrix.Color333(7, 0, 0));
+   matrix.drawLine(1, 30, 18, 30, matrix.Color333(7, 0, 0));
+
+   //Upper right lopsided bar
+   matrix.drawLine(7, 16, 23, 16, matrix.Color333(7, 0, 0));
+   matrix.drawLine(7, 17, 23, 17, matrix.Color333(7, 0, 0));
+   matrix.drawLine(7, 18, 12, 18, matrix.Color333(7, 0, 0));
+   matrix.drawLine(20, 18, 23, 18, matrix.Color333(7, 0, 0));
+   matrix.drawLine(20, 19, 23, 19, matrix.Color333(7, 0, 0));
+
+   //Upper right block
+   matrix.drawLine(14, 19, 18, 19, matrix.Color333(7, 0, 0));
+   matrix.drawLine(14, 20, 18, 20, matrix.Color333(7, 0, 0));
+   matrix.drawLine(14, 21, 18, 21, matrix.Color333(7, 0, 0));
+   matrix.drawLine(14, 22, 18, 22, matrix.Color333(7, 0, 0));
+   matrix.drawLine(14, 23, 18, 23, matrix.Color333(7, 0, 0));
+   matrix.drawLine(14, 24, 18, 24, matrix.Color333(7, 0, 0));
+   matrix.drawLine(14, 25, 18, 25, matrix.Color333(7, 0, 0));
+
+   //Upper left bottom left block
+   matrix.drawLine(26, 16, 30, 16, matrix.Color333(7, 0, 0));
+   matrix.drawLine(26, 17, 30, 17, matrix.Color333(7, 0, 0));
+   matrix.drawLine(26, 18, 30, 18, matrix.Color333(7, 0, 0));
+   matrix.drawLine(30, 19, 30, 25, matrix.Color333(7, 0, 0));
+   matrix.drawLine(29, 25, 25, 25, matrix.Color333(7, 0, 0));
+
+   //Upper left middle block
+   matrix.drawLine(25, 20, 28, 20, matrix.Color333(7, 0, 0));
+   matrix.drawLine(21, 21, 28, 21, matrix.Color333(7, 0, 0));
+   matrix.drawLine(21, 22, 28, 22, matrix.Color333(7, 0, 0));
+   matrix.drawLine(21, 23, 28, 23, matrix.Color333(7, 0, 0));
+
+   //Upper left upper right block
+   matrix.drawLine(21, 26, 23, 26, matrix.Color333(7, 0, 0));
+   matrix.drawLine(21, 27, 23, 27, matrix.Color333(7, 0, 0));
+   matrix.drawLine(21, 28, 23, 28, matrix.Color333(7, 0, 0));
+   matrix.drawLine(21, 29, 23, 29, matrix.Color333(7, 0, 0));
+   matrix.drawLine(21, 30, 23, 30, matrix.Color333(7, 0, 0));
+
+   //Upper left upper left block
+   matrix.drawLine(26, 27, 30, 27, matrix.Color333(7, 0, 0));
+   matrix.drawLine(26, 28, 30, 28, matrix.Color333(7, 0, 0));
+   matrix.drawLine(26, 29, 30, 29, matrix.Color333(7, 0, 0));
+   matrix.drawLine(26, 30, 30, 30, matrix.Color333(7, 0, 0));
 }
 
 void setup() 
@@ -85,6 +151,8 @@ void loop()
 { 
   if(Serial.available() > 0)
     recieveUSART();
+  
   drawPlayer();
+  
   matrix.swapBuffers(false);
 }
